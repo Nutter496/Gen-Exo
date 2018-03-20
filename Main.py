79302c139cb534f77532a5c5060b294af992f8ab
Matt Nutter <matt.g.nutter@gmail.com>
2018-03-20 15:19:40 +0000

    Began reducing the large arrays by using loadtxt function from numpy (Main.py) Added a row to Nplan_per_star.dat full of zeroes to ease code in Main.py

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@
 #==================================================
 from random import *
 import numpy as np
+from numpy import loadtxt
 import Seed				# Calls an object creating module for stars
 import Seed_Plan			# calls an object creating module for planets
 #==================================================
@@ -48,46 +49,45 @@ count_p = 0
 # the star's properties (radius, temp, MS lifetime,
 # stellar type, etc)
 #==================================================
-print "Generate new stellar population data (s) or new planetary data (p)?"
-l = raw_input()
-if l == 's':
-	fstar1 = open('Stars.dat','w')
-	fstar1.write('ID\t' + 'Mass\t' + '[Fe/H]\t' + 'Radius\t' + 'Type\t' + 'Teff\t' + 'Tms\t' + 'Nplan\t' + '\n')
-	f2 = open('Nplan_per_star.dat','r')	# Data from Dressing & Charbonneau 2013
-	row = f2.readlines()
-	cum_freq = row[-1].split( )[-1]
-	Nplan_bins = [0,int(row[1].split()[-1]),int(row[2].split()[-1]),int(row[3].split()[-1]),int(row[4].split()[-1]),int(row[5].split()[-1])]
-	f3 = open('Type.dat','r')		# Type categorisedby masses
-	type_line = f3.readlines()
-	upper = [type_line[1].split()[-1],type_line[2].split()[-1],type_line[3].split()[-1],type_line[4].split()[-1],type_line[5].split()[-1]]
-	lower = [type_line[1].split()[-2],type_line[2].split()[-2],type_line[3].split()[-2],type_line[4].split()[-2],type_line[5].split()[-2]]
+fstar1 = open('Stars.dat','w')
+fstar1.write('ID\t' + 'Mass\t' + '[Fe/H]\t' + 'Radius\t' + 'Type\t' + 'Teff\t' + 'Tms\t' + 'Nplan\t' + '\n')
+fplan = open('Planets.dat','w')
+fplan.write('ID\t' + 'Mass\t' + 'Radius\t' + 'e\t' + 'a\t' + 'Torb\t' + 'Incl\t' + '\n')
 
-	with open('Stellar_Popl.dat') as f1:		# Data file with mass and metallicity for a stellar population
-		for line in f1:
-			Input = map(float,line.split())
-			star = Seed.star
-			count_s +=  1
-			star.ID = count_s
-			star.mass = Input[0]
-			star.metal = Input[1]
-			star.radius = Input[0]**(3.0/7)	# Relation due to p-p chain fusion
-			star.temp = star.mass*5700	# Relation due to 0.43Ms < M* < 2Ms
-			star.Tms = 10*star.mass**(-3)	# Relation due to M* ~ Ms
-			RaNdOm = randint(1,int(cum_freq))
-			for i in range(len(planet_count)):
-				if Nplan_bins[i] < RaNdOm <= Nplan_bins[i+1]:	
-					star.Nplan = row[i+1].split()[0]	
-					if (i+1) == int(star.Nplan):
-						planet_count[i]=planet_count[i]+1
-				if lower[i] < str(star.mass) <= upper[i]:
-					star.type = type_line[i+1].split()[0]
-					type_count[i] += 1 
-			fstar1.write(star.description() + '\n')
-	print "Stellar type count =",type_count 	# Redundant atm as they're all sun clones
-	print "Planet count =",planet_count
-	f1.close()
-	f2.close()
-	fstar1.close()
+N_p_star = loadtxt("Nplan_per_star.dat", comments="#", delimiter="\t",unpack=False)	# Data from Dressing & Charbonneau 2013
+cum_freq = N_p_star[-1,-1]
+#type_dat = loadtxt("Type.dat", comments="#", delimiter="\t",unpack=False)
+f3 = open('Type.dat','r')		# Type categorised by masses
+type_line = f3.readlines()
+upper = [type_line[1].split()[-1],type_line[2].split()[-1],type_line[3].split()[-1],type_line[4].split()[-1],type_line[5].split()[-1]]
+lower = [type_line[1].split()[-2],type_line[2].split()[-2],type_line[3].split()[-2],type_line[4].split()[-2],type_line[5].split()[-2]]
+
+
+with open('Stellar_Popl.dat') as f1:		# Data file with mass and metallicity for a stellar population
+	for line in f1:
+		Input = map(float,line.split())
+		star = Seed.star
+		count_s +=  1
+		star.ID = count_s
+		star.mass = Input[0]
+		star.metal = Input[1]
+		star.radius = Input[0]**(3.0/7)	# Relation due to p-p chain fusion
+		star.temp = star.mass*5700	# Relation due to 0.43Ms < M* < 2Ms
+		star.Tms = 10*star.mass**(-3)	# Relation due to M* ~ Ms
+		RaNdOm = randint(1,int(cum_freq))
+		for i in range(len(planet_count)):
+			if N_p_star[i,2] < RaNdOm <= N_p_star[i+1,2]:	
+				star.Nplan = N_p_star[i+1,0]	
+				if (i+1) == int(star.Nplan):
+					planet_count[i] += 1
+			if lower[i] < str(star.mass) <= upper[i]:
+				star.type = type_line[i+1].split()[0]
+				type_count[i] += 1 
+		fstar1.write(star.description() + '\n')
+print "Stellar type count =",type_count 	# Redundant atm as they're all sun clones
+print "Planet count =",planet_count
+f1.close()
+fstar1.close()
 #==================================================
 
 
@@ -116,37 +116,34 @@ if l == 's':
 # Ra = Radius of planet 'a'
 # Da = Density of planet 'a'
 #==================================================
-if (l == 'p') or (l == 's') :
-	fstar2 = open('Stars.dat','r').readlines()
-	fplan = open('Planets.dat','w')
-	fplan.write('ID\t' + 'Mass\t' + 'Radius\t' + 'e\t' + 'a\t' + 'Torb\t' + 'Incl\t' + '\n')
-	star_ID = []
-	star_mass = []
-	star_Nplan = []
-	plan_ID = []
-	plan = Seed_Plan.plan
-	for i in range(len(fstar2)-1):
-		star_ID.append(fstar2[i+1].split()[0])
-		star_mass.append(fstar2[i+1].split()[1])
-		star_Nplan.append(fstar2[i+1].split()[-1])
-	star_ID = np.array(star_ID)
-	star_mass = map(float, np.array(star_mass)[:])
-	star_Nplan = np.array(star_Nplan)
-	for i in range(len(fstar2)-1):
-		n = 0
-		while n in range(int(star_Nplan[i])):
-			plan.ID = str(star_ID[i]) + alphabet[n]
-			plan.mass = randint(1,20)*1.0/randint(1,19)
-			plan.radius = plan.mass*3.0*random()
-			plan.ecc = random()
-			plan.sm_axis = random()+0.1
-			plan.t_orb = ((4*pi*(AU*plan.sm_axis)**3/(G*Msun*star_mass[i]))**0.5)/(Day)
-			plan.incl = 180*(random()-0.5)
-			n += 1
-			fplan.write(plan.description() + '\n')
-			count_p += 1
-	print count_p
-	fplan.close()
+#fstar2 = open('Stars.dat','r').readlines()
+#star_ID = []
+#star_mass = []
+#star_Nplan = []
+#plan_ID = []
+#plan = Seed_Plan.plan
+#for i in range(len(fstar2)-1):
+#	star_ID.append(fstar2[i+1].split()[0])
+#	star_mass.append(fstar2[i+1].split()[1])
+#	star_Nplan.append(fstar2[i+1].split()[-1])
+#star_ID = np.array(star_ID)
+#star_mass = map(float, np.array(star_mass)[:])
+#star_Nplan = np.array(star_Nplan)
+#for i in range(len(fstar2)-1):
+#	n = 0
+#	while n in range(int(star_Nplan[i])):
+#		plan.ID = str(star_ID[i]) + alphabet[n]
+#		plan.mass = randint(1,20)*1.0/randint(1,19)
+#		plan.radius = plan.mass*3.0*random()
+#		plan.ecc = random()
+#		plan.sm_axis = random()+0.1
+#		plan.t_orb = ((4*pi*(AU*plan.sm_axis)**3/(G*Msun*star_mass[i]))**0.5)/(Day)
+#		plan.incl = 180*(random()-0.5)
+#		n += 1
+#		fplan.write(plan.description() + '\n')
+#		count_p += 1
+#print count_p
+#fplan.close()
 #==================================================
 
 
